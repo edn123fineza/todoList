e3cd5d4932e784c006532e7967205fb9a402acec
eden fineza <edengfineza@gmail.com>
2024-07-30 09:49:59 -0700

    Initial Commit

---
+++ todoList/todoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListApp.swift
+//  todoList
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct todoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
